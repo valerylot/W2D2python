4003c10dffe547f0371a6f38532c5e898d6f429e
valerylot <lotvalery@gmail.com>
2024-01-16 08:58:00 -0800

    edited python code

--- W2D2python.py
+++ W2D2python.py
@@ -12,26 +12,28 @@ arr3 = []
 expected3 = {}
 
 
-def makeFrequencyTable(arr):
-    empty = {}
-    for i in arr:
-        # print(empty[i])
-        if empty.get(i) is None:
-            empty[i] = 1
+def make_frequency_table(arr):
+    frequency_table = {}
+
+    for val in arr:
+        if val not in frequency_table:
+            frequency_table[val] = 1
+        else:
+            frequency_table[val] += 1
+
+    return frequency_table
 
 
+arr1 = ["a", "a", "a"]
+expected1 = {"a": 3}
 
+arr2 = ["a", "b", "a", "c", "B", "c", "c", "d"]
+expected2 = {"a": 2, "b": 1, "c": 3, "B": 1, "d": 1}
 
+arr3 = []
+expected3 = {}
 
-
-    # i=0
-    # while i < arr:
-    #     print(arr[i])
-    #     i+=1
-
-
-makeFrequencyTable(arr1)
-makeFrequencyTable(arr2)
-makeFrequencyTable(arr3)
-
+print(make_frequency_table(arr1))
+print(make_frequency_table(arr2))
+print(make_frequency_table(arr3))
 
